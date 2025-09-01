6924120084abcee48194bfab83872c5f5c0178ee
surendracngh <surendracngh@gmail.com>
2025-09-01 19:49:37 +0545

    task

--- mode.py
+++ mode.py
@@ -3,6 +3,6 @@ from scipy import stats
 
 arr = np.array([1, 2, 2, 3, 3, 3, 4, 5])
 
-mode_result = stats.mode(arr)
+mode_result = stats.mode(arr, keepdims=True)
 print("Mode:", mode_result.mode[0])
 print("Count:", mode_result.count[0])
--- suren.py
+++ suren.py
@@ -1,4 +1,5 @@
 import numpy as np
 
+
 arr=np.empty(5)
 print(arr)
